4e2a4e9da9e304883928d64195f9d4894e9b81a6
dharanicb <dharanicb86391@gmail.com>
2023-04-01 04:18:49 +0000

    solution recent

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const Box = (props) => {
   //  Write your code here.
   const { boxProperties, textContent } = props;
   return (
-  <div className= {`box ${boxProperties}`}>
-     <p className = "text-content"> {textContent} </p>
-  </div>
-  )
+    <div className={`box ${boxProperties}`}>
+      <p className="text-content"> {textContent} </p>
+    </div>
+  );
 };
 
 const element = (
   //  Write your code here.
-  
-  <h1>Boxes</h1>
-  <div className "flex-container">
-  <Box boxProperties="properties" textContent="Small"/>
-  <Box boxProperties="properties1" textContent="Medium"/>
-  <Box boxProperties="properties2" textContent="Large"/>
+  <div>
+    <h1 className="heading">Boxes</h1>
+    <div className="flex-container">
+      <Box boxProperties="properties" textContent="Small" />
+      <Box boxProperties="properties1" textContent="Medium" />
+      <Box boxProperties="properties2" textContent="Large" />
+    </div>
   </div>
 );
 
